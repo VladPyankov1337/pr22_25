ae664a600cfbb181cc1294727e27f72a3e682c52
VladPyankov1337 <vladipyankov@yandex.ru>
2025-10-29 00:32:33 +0500

    Получение данных о категориях. Пьянков

--- Data/DataBase/DBCategory.cs
+++ Data/DataBase/DBCategory.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using MySql.Data.MySqlClient;
+using Shop.Data.Common;
+using Shop.Data.Interfaces;
+using Shop.Data.Models;
+using System.Collections.Generic;
+
+namespace Shop.Data.DataBase
+{
+    public class DBCategory : ICategorys
+    {
+        public IEnumerable<Categorys> AllCategorys
+        {
+            get
+            {
+                List<Categorys> categorys = new List<Categorys>();
+                MySqlConnection MySqlConnection = Connection.OpenConnection();
+                MySqlDataReader CategorysData = Connection.Query("SELECT * FROM Shop.Categorys ORDER BY `Name`;", MySqlConnection);
+                while (CategorysData.Read())
+                {
+                    categorys.Add(new Categorys()
+                    {
+                        Id = CategorysData.IsDBNull(0) ? -1 : CategorysData.GetInt32(0),
+                        Name = CategorysData.IsDBNull(1) ? null : CategorysData.GetString(1),
+                        Description = CategorysData.IsDBNull(2) ? null : CategorysData.GetString(2)
+                    });
+                }
+                return categorys;
+            }
+        }
+    }
+}
