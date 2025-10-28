a32099fcde597a42bd5a162abaf0e8c1e3b3bfb2
VladPyankov1337 <vladipyankov@yandex.ru>
2025-10-29 00:07:26 +0500

    Разработанные модели данных. Пьянков

--- Data/Models/Items.cs
+++ Data/Models/Items.cs
@@ -1,1 +1,13 @@
-﻿
+﻿namespace Shop.Data.Models
+{
+    public class Items
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+        public int CategoryId { get; set; }
+        public Categorys Category { get; set; }
+    }
+}
