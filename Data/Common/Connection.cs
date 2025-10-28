12516902ecdb132062b4c63726242156329367c4
VladPyankov1337 <vladipyankov@yandex.ru>
2025-10-29 00:29:53 +0500

    Разработанное подключение к MySql. Пьянков

--- Data/Common/Connection.cs
+++ Data/Common/Connection.cs
@@ -1,1 +1,39 @@
-﻿
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Diagnostics;
+
+namespace Shop.Data.Common
+{
+    public class Connection
+    {
+        readonly static string connection = "server=localhost;port=3306;database=Shop;user=root;pwd=;";
+        public static MySqlConnection OpenConnection()
+        {
+            try
+            {
+                MySqlConnection mySqlConnection = new MySqlConnection(connection);
+                mySqlConnection.Open();
+                return mySqlConnection;
+            }
+            catch (Exception exp)
+            {
+                Debug.WriteLine(exp.Message);
+                return null;
+            }
+        }
+        public static MySqlDataReader Query(string Sql, MySqlConnection mySqlConnection)
+        {
+            MySqlCommand mySqlCommand = new MySqlCommand(Sql, mySqlConnection);
+            return mySqlCommand.ExecuteReader();
+        }
+        public static void CloseConnection(MySqlConnection mySqlConnection)
+        {
+            mySqlConnection.Close();
+            MySqlConnection.ClearPool(mySqlConnection);
+        }
+        public static bool OpenConnection(MySqlConnection mySqlConnection)
+        {
+            return mySqlConnection != null && mySqlConnection.State == System.Data.ConnectionState.Open;
+        }
+    }
+}
